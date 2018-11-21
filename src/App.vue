ddf2fc4d6555e0bdf84a65e99cb6d02c4b329902
Hobbytowo <weronika.ak@wp.pl>
2018-11-21 22:37:23 +0100

    Add form component

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,36 @@
 <template>
   <div class="">
     <h1 class="title">Statistical Process Control - control chart</h1>
+    <form-component @createChart="createChart"/>
     <div class="container">
-      {{ points }}
+      points: {{ points }}
     </div>
   </div>
 </template>
 
 <script>
 import _ from 'lodash'
+import formComponent from '@/components/form'
 
 export default {
   name: 'app',
+  components: {
+    formComponent
+  },
   data () {
     return {
       data: new Array(600).fill().map(() => Math.round(Math.random() * 10000) / 100),
-      amountOfDataInPoint: 6,
-      amountOfPointToCreateChart: 7
+      points: [],
+      mean: 0
     }
   },
-  computed: {
-    points () {
-      return _.chunk(this.data, this.amountOfDataInPoint).map(data => {
+  methods: {
+    createChart (dataAmount, pointsAmount) {
+      this.points = _.chunk(this.data, dataAmount).map(data => {
         return Math.round(_.mean(data) * 100) / 100
       })
-    },
-    mean () {
-      return Math.round(_.mean(this.points.splice(0, this.amountOfPointToCreateChart)) * 100) / 100
+
+      this.mean = Math.round(_.mean(this.points.splice(0, pointsAmount)) * 100) / 100
     }
   }
 }
@@ -42,5 +46,6 @@ export default {
   .title {
     text-align: center;
     font-weight: 700;
+    margin-bottom: 40px;
   }
 </style>
--- src/components/form.vue
+++ src/components/form.vue
@@ -1,11 +1,85 @@
 <template lang="html">
+  <div class="form">
+    <div class="row">
+      <div class="data">
+        <label class="label" for="dataAmount">Amount of data in one point:</label>
+        <input v-model="dataAmount" class="input" type="number" min="0" id="dataAmount">
+      </div>
 
+      <div class="data">
+        <label class="label" for="pointsAmount">Amount of points to create chart:</label>
+        <input v-model="pointsAmount" class="input" type="number" min="0" id="pointsAmount">
+      </div>
+    </div>
+    <button @click="createChart" class="button" type="button" name="button">
+      Create chart
+    </button>
+  </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      dataAmount: 6,
+      pointsAmount: 8
+    }
+  },
+  methods: {
+    createChart () {
+      this.$emit('createChart', this.dataAmount, this.pointsAmount)
+    }
+  }
 }
 </script>
 
-<style lang="css" scoped>
+<style lang="scss" scoped>
+  .form {
+    margin-bottom: 40px;
+  }
+
+  .row {
+    display: flex;
+    justify-content: space-around;
+  }
+
+  .data {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .label {
+    margin-bottom: 15px;
+  }
+
+  .input {
+    width: 50px;
+    background-color: #fff99b;
+    padding: 7px;
+    border: 2px solid black;
+    border-radius: 5px;
+    margin-left: 10px;
+  }
+
+  .button {
+    border-radius: 15px;
+    border: 2px solid #fff;
+    padding: 10px 15px;
+    display: block;
+    margin: auto;
+    text-transform: uppercase;
+    letter-spacing: 2px;
+    font-weight: bold;
+    background-color: #003a2e;
+    color: #fff;
+
+    cursor: pointer;
+    transition: all 0.3s;
+    will-change: background-color;
+
+    &:hover {
+      background-color: #00163a;
+    }
+  }
 </style>
