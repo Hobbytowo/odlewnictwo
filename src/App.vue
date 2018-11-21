58f0fa01d5782c3bd76a128760131dc2b303d88f
Hobbytowo <weronika.ak@wp.pl>
2018-11-21 20:36:41 +0100

    Add basic data

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,44 @@
 <template>
-  <div id="app">
+  <div class="">
+    <h1 class="title">Statistical Process Control - control chart</h1>
+    <div class="container">
+      {{ points }}
+    </div>
   </div>
 </template>
 
 <script>
+import _ from 'lodash'
 
 export default {
   name: 'app',
-  components: {
+  data () {
+    return {
+      data: new Array(600).fill().map(() => Math.round(Math.random() * 10000) / 100),
+      amountOfDataInPoint: 6,
+      amountOfPointToCreateChart: 7
+    }
+  },
+  computed: {
+    points () {
+      return _.chunk(this.data, this.amountOfDataInPoint).map(data => {
+        return Math.round(_.mean(data) * 100) / 100
+      })
+    },
+    mean () {
+      return Math.round(_.mean(this.points.splice(0, this.amountOfPointToCreateChart)) * 100) / 100
+    }
   }
 }
 </script>
 
-<style>
+<style lang="css">
+  body {
+    background-color: #444;
+    color: #eee;
+  }
+
+  .title {
+    text-align: center;
+  }
 </style>
