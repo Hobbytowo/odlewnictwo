f0b1b993a23ada0bc2122658ad0a8d89a0d25cd6
Hobbytowo <weronika.ak@wp.pl>
2019-01-07 00:38:31 +0100

    Add checkRule2 function

--- src/assets/js/rules.js
+++ src/assets/js/rules.js
@@ -1,7 +1,7 @@
 // Western Electric rules
 // https://en.wikipedia.org/wiki/Western_Electric_rules
 
-const data = [2,5,8,8,85,4,4,5,5,8,8,7]
+const data = [2,5,8,8,85, 10,4,4,5,5,8,8,7]
 
 function createArrays (data, numberOfDataInArr) {
   const arr = []
@@ -19,8 +19,6 @@ function createArrays (data, numberOfDataInArr) {
   return arr
 }
 
-console.log(createArrays(data, 4))
-
 // rule 1 - Any single data point falls outside the 3σ-limit
 export function checkRule1 (values) {
   return values.filter(x => (x > this.valueUCL || x < this.valueLCL))
@@ -28,7 +26,36 @@ export function checkRule1 (values) {
 // e.o rule 1
 
 // rule 2 - Two out of three consecutive points fall beyond the 2σ-limit
+const zoneAupperBottom = 8
+const zoneAlowerTop = 3
+
 export function checkRule2 (values) {
-  return values.filter(x => (x > this.valueUCL || x < this.valueLCL))
+  const isBeyondZoneAUpper = point => point > zoneAupperBottom
+  const isBeyondZoneALower = point => point < zoneAlowerTop
+
+  const checkRuleOneUpper = arr => {
+    let missed = 0
+    arr.map(point => {
+      isBeyondZoneAUpper(point) && missed++
+    })
+
+    return missed
+  }
+
+  const checkRuleOneLower = arr => {
+    let missed = 0
+    arr.map(point => {
+      isBeyondZoneALower(point) && missed++
+    })
+
+    return missed
+  }
+
+  const arr3 = createArrays(values, 3)
+  return arr3.filter(arr => {
+    return checkRuleOneUpper(arr) < 2 && checkRuleOneLower(arr) < 2
+  })
 }
+
+console.log(checkRule2(data))
 // e.o rule 2
