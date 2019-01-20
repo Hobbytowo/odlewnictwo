4301349dbcc8fc0c3e87b7dc2d0bb49f2e450ade
Hobbytowo <weronika.ak@wp.pl>
2019-01-20 20:09:41 +0100

    Add form-csv component to upload csv files

---
+++ src/components/form-csv.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+  <input type="file" @change="updateData($event)">
+</template>
+
+<script>
+export default {
+  methods: {
+    updateData(e) {
+      if (window.FileReader) {
+        const reader = new FileReader()
+        reader.readAsText(e.target.files[0])
+
+        reader.onload = event => {
+          const csv = event.target.result
+          console.log(csv)
+        }
+        reader.onerror = event => {
+          if (evt.target.error.name === "NotReadableError") {
+            alert("Canno't read file !")
+          }
+        }
+      } else {
+        alert('FileReader are not supported in this browser.')
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
