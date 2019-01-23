784dc5b0e73a26763ccef37e5f253cc0c7b24819
Hobbytowo <weronika.ak@wp.pl>
2019-01-24 00:25:55 +0100

    Add files watcher

--- src/components/form-csv.vue
+++ src/components/form-csv.vue
@@ -1,14 +1,30 @@
 <template lang="html">
   <div class="">
-    <input type="file" @change="updateData($event)">
+    <button
+      ref="start"
+      type="button"
+      @click="startWatching"
+      v-text="'Select folder to watch'"
+    />
+
+    <button
+      ref="stop"
+      type="button"
+      @click="stopWatching"
+      v-text="'Stop watching'"
+    />
   </div>
 </template>
 
 <script>
+import chokidar from 'chokidar'
+import fs from 'fs'
+
 export default {
   data () {
     return {
-      data: []
+      data: [],
+      watcher: null
     }
   },
   methods: {
@@ -32,6 +48,64 @@ export default {
         .map(data => data.replace('\r', ''))
         .filter(data => data)
         .map(data => data.replace(',', '.') * 1)
+    },
+    startWatcher (path) {
+      this.watcher = chokidar.watch(path, {
+        ignored: /[\/\\]\./,
+        persistent: true
+      })
+
+      function onWatcherReady() {
+        console.log('From here can you check for real changes, the initial scan has been completed.')
+      }
+
+      this.watcher
+      .on('add', function (path) {
+        console.log('File', path, 'has been added')
+      })
+      .on('addDir', function (path) {
+         console.log('Directory', path, 'has been added')
+       })
+      .on('change', function (path) {
+        console.log('File', path, 'has been changed')
+      })
+      .on('unlink', function (path) {
+        console.log('File', path, 'has been removed')
+      })
+      .on('unlinkDir', function (path) {
+        console.log('Directory', path, 'has been removed')
+      })
+      .on('error', function (error) {
+        console.log('Error happened', error)
+      })
+      .on('ready', onWatcherReady)
+      .on('raw', function(event, path, details) {
+        // This event should be triggered everytime something happens.
+        console.log('Raw event info:', event, path, details)
+      })
+    },
+    startWatching () {
+      const { dialog } = require('electron').remote
+      const vm = this
+
+      dialog.showOpenDialog({
+        properties: ['openDirectory']
+      }, function (path) {
+        if (path) {
+          vm.startWatcher(path[0])
+        } else {
+          console.log("No path selected")
+        }
+      })
+    },
+    stopWatching () {
+      if (!this.watcher) {
+        console.log("You need to start first the watcher")
+      } else {
+        this.watcher.close()
+        this.$refs.start.disabled = false
+        console.log("Nothing is being watched")
+      }
     }
   },
   watch: {
