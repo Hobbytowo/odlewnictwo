c9f268270940374b17a035112d98ef15ff7a5543
Hobbytowo <weronika.ak@wp.pl>
2019-02-05 02:23:16 +0100

    Add function to check rule 6 - alternating numbers

--- src/assets/js/rulesForLastPoints.js
+++ src/assets/js/rulesForLastPoints.js
@@ -6,7 +6,7 @@ export default function checkRules (store) {
 
   // rule 1 - Any single data point falls outside the 3σ-limit
   const checkRule1 = () => {
-    const point = points[points.length - 1]
+    const point = points.slice(-1)
     if (point > store.getters.valueUCL
      || point < store.getters.valueLCL) {
       alert('Break rule 1')
@@ -16,8 +16,7 @@ export default function checkRules (store) {
 
   // rule 2 - Two out of three consecutive points fall beyond the 2σ-limit
   const checkRule2 = () => {
-    const arrToTest = []
-    points.map((x, i) => i > points.length - 4 && arrToTest.push(x))
+    const arrToTest = points.slice(-3)
 
     const checkUpper = arr => {
       let missed = 0
@@ -46,8 +45,7 @@ export default function checkRules (store) {
 
   // rule 3 - Four out of five consecutive points fall beyond the 1σ-limit
   const checkRule3 = () => {
-    const arrToTest = []
-    points.map((x, i) => i > points.length - 6 && arrToTest.push(x))
+    const arrToTest = points.slice(-5)
 
     const checkUpper = arr => {
       let missed = 0
@@ -76,8 +74,7 @@ export default function checkRules (store) {
 
   // rule 4 - Nine consecutive points fall on the same side of the centerline (in zone C or beyond)
   const checkRule4 = () => {
-    const arrToTest = []
-    points.map((x, i) => i > points.length - 10 && arrToTest.push(x))
+    const arrToTest = points.slice(-9)
     const center = store.getters.centerValue
 
     if (arrToTest.every(point => point > center)
@@ -89,8 +86,7 @@ export default function checkRules (store) {
 
   // rule 5 - Six consecutive points are steadil increasing or decreasing
   const checkRule5 = () => {
-    const arrToTest = []
-    points.map((x, i) => i > points.length - 7 && arrToTest.push(x))
+    const arrToTest = points.slice(-6)
 
     const checkIncreasing = arr => {
       let increasing = 0
@@ -118,11 +114,38 @@ export default function checkRules (store) {
   }
   // e.o rule 5
 
+  // rule 6 - 14 alternating up/down
+  const checkRule6 = () => {
+    const arrToTest = points.slice(-14)
+
+    const ifNextIsLower = arr => {
+      const decreasing = []
+      arr.map((point, idx) => {
+        if (idx !== 13) {
+          decreasing.push(point > arrToTest[idx + 1])
+      }})
+
+      return decreasing
+    }
+
+    const booleanArray = ifNextIsLower(arrToTest)
+
+    const firstTrueValuesTrue = booleanArray.filter((x, idx) => x && !(idx % 2))
+    const firstTrueValuesFalse = booleanArray.filter((x, idx) => !x && (idx % 2))
+
+    if ((firstTrueValuesTrue.length === 7 && firstTrueValuesFalse.length === 6)
+    || (firstTrueValuesTrue.length === 0 && firstTrueValuesFalse.length === 0)) {
+      alert('Break rule 6')
+    }
+  }
+  // e.o rule 6
+
   // invoke functions
   points.length > 0 && checkRule1()
   points.length > 2 && checkRule2()
   points.length > 4 && checkRule3()
   points.length > 8 && checkRule4()
   points.length > 5 && checkRule5()
+  points.length > 13 && checkRule6()
   // e/o invoke functions
 }
