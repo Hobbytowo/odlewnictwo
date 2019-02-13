fa169101690bdea4e282f6159f7a0510f9aa2e31
Wer <weronika.ak@wp.pl>
2019-02-13 23:17:41 +0100

    Dynamic render points colors for broken rules

--- src/assets/js/chart.js
+++ src/assets/js/chart.js
@@ -1,4 +1,4 @@
-import { Bar } from 'vue-chartjs'
+import {Bar} from 'vue-chartjs'
 // eslint-disable-next-line
 import chartjsPluginAnnotation from "chartjs-plugin-annotation";
 // eslint-disable-next-line
@@ -11,86 +11,97 @@ import checkRulesForLastPoints from '@/assets/js/rulesForLastPoints'
 export default {
   extends: Bar,
   computed: {
-    chartData () {
+    chartData() {
       return this.$store.getters.pointsToTest
     },
-    windowWidth () {
+    windowWidth() {
       return this.$store.state.windowWidth
     },
-    rules () {
+    rules() {
       return this.$store.state.rules
     },
-    pointsColors () {
-      return this.chartData.map(x => {
-        return (x > this.$store.getters.valueUCL || x < this.$store.getters.valueLCL)
+    brokenRules () {
+      return this.$store.state.brokenRules
+    },
+    maxBrokenData () {
+      let nrOfData = []
+      this.brokenRules.forEach(brokenRuleIdx => {
+        nrOfData.push(this.rules[brokenRuleIdx - 1].nrOfData)
+      })
+
+      return Math.max(...nrOfData)
+    },
+    pointsColors() {
+      return this.chartData.map((x, idx) => {
+        return idx > this.chartData.length - this.maxBrokenData
           ? 'red'
           : 'green'
       })
     }
   },
-  mounted () {
+  mounted() {
     this.renderSpcChart()
   },
   methods: {
-    renderSpcChart () {
+    renderSpcChart() {
       // if there is not enough data
       if (!this.$store.getters.isEnoughData) {
         showMessage(this, 'Not enough data to create chart')
-        this.renderChart({ labels: []}, { maintainAspectRatio: false })
+        this.renderChart({labels: []}, {maintainAspectRatio: false})
 
         return
       }
       // e/o if there is not enough data
 
       // if there is enough data
+      checkRulesForLastPoints(this.$store)
+
       this.renderChart({
-        labels: new Array(this.chartData.length).fill().map((x, i) => i + 1),
-        datasets: [
-          {
-            type: 'scatter',
-            label: 'point',
-            pointBackgroundColor: this.pointsColors,
-            backgroundColor: 'transparent',
-            data: this.chartData
-          }
-        ],
-      },
-      // options
-      {
-        responsive: true,
-        maintainAspectRatio: false,
-        legend: { display: false },
-        annotation: createAnnotations(this.$store.getters),
-        // zoom
-        scales: {
-          xAxes: [{
-            ticks: {
-              beginAtZero: true,
-              min: this.chartData.length - this.windowWidth - 1,
-              max: this.chartData.length,
-              stepSize: 0.4
-            },
-            scaleLabel: {
-              display: true
+          labels: new Array(this.chartData.length).fill().map((x, i) => i + 1),
+          datasets: [
+            {
+              type: 'scatter',
+              label: 'point',
+              pointBackgroundColor: this.pointsColors,
+              backgroundColor: 'transparent',
+              data: this.chartData
             }
-          }]
+          ],
         },
-        pan: {
-          enabled: true,
-          mode: 'x'
-        },
-        zoom: {
-          enabled: true,
-          mode: 'x',
-        },
-        // e.o zoom
-      })
+        // options
+        {
+          responsive: true,
+          maintainAspectRatio: false,
+          legend: {display: false},
+          annotation: createAnnotations(this.$store.getters),
+          // zoom
+          scales: {
+            xAxes: [{
+              ticks: {
+                beginAtZero: true,
+                min: this.chartData.length - this.windowWidth - 1,
+                max: this.chartData.length,
+                stepSize: 0.4
+              },
+              scaleLabel: {
+                display: true
+              }
+            }]
+          },
+          pan: {
+            enabled: true,
+            mode: 'x'
+          },
+          zoom: {
+            enabled: true,
+            mode: 'x',
+          },
+          // e.o zoom
+        })
       // e/o options
       // e/o if there is enough data
-
-      checkRulesForLastPoints(this.$store)
     },
-    rerender () {
+    rerender() {
       if (this.$store.getters.isEnoughData) {
         clearChart(this)
         this.$data._chart.destroy()
@@ -99,14 +110,17 @@ export default {
     }
   },
   watch: {
-    chartData () {
+    chartData() {
       this.rerender()
     },
-    windowWidth () {
+    windowWidth() {
       this.rerender()
     },
-    rules () {
+    rules() {
       this.rerender()
     }
+    // brokenRules () {
+    //   this.rerender()
+    // }
   }
 }
